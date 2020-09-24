027ae131d3276f5246c05e4e11c8ef626b932f99
Luca Genova <48278825+Lucajett99@users.noreply.github.com>
2020-09-24 14:21:35 +0200

    Control Home

--- Progetto tec_web/src/src_control/ControlHome.js
+++ Progetto tec_web/src/src_control/ControlHome.js
@@ -1,8 +1,45 @@
 const e = React.createElement;
 
+function Card(props){
+    return(
+        e("div",{style: {width: "100px", height: "150px", border: "3px solid black", float: "left", margin: "5px"}},[
+            e("div", {id: "header_card", style: {"font-size": "13px", "border-bottom": "1px solid black", height: "25%"}},"Nome/Id: "),
+            e("div", {id: "body_card", style: {"font-size": "13px", "border-bottom": "1px solid black", height: "25%"}}, "Sezione: "),
+            e("div", {id: "body_card", style: {"font-size": "13px", "border-bottom": "1px solid black", height: "25%"}}, "Punteggio: "),
+            e("div", {id: "footer_card"},[
+                e("input",{type: "button", value: "Chat", style: {"font-size": "13px", "margin-left": "24%", "margin-top": "10%"}}, )
+            ])
+        ])
+    )
+}
 
 function controlHome(props){
-    return e("div", null, "CONTROL")
+    var arr = [];
+
+    /*fetch(`https://api.github.com/search/repositories?q=user:its-hmny&sort=updated`)
+        .then(response => response.json())
+        .then(data => { 
+            for(let i=0 ; i < data.total_count.lenght; i++)
+                arr.push( e(Card, {key: data.items[i].id}) );
+                
+        }).catch((error) => console.log(error));
+        */
+
+       for(let i=0 ; i < 30; i++)
+            arr.push(e(Card,null));
+
+    return e(React.Fragment, null, [
+        e("div",null,[
+        arr
+        ]),
+        e("div",null,[
+            e("a", {href: "#ex1", rel: "modal:open"}, "Open Modal"),
+            e("div", {className: "reservation modal", id: "ex1"}),
+            e("a", {href: "#", rel: "modal:close"}, "Close")
+        ])
+    ]); 
+
+   
 }
 
 export default controlHome;

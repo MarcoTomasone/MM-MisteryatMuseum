3b60977441f83288de6272aa0450b5ffffd7d7d1
Luca Genova <48278825+Lucajett99@users.noreply.github.com>
2020-09-26 12:57:42 +0200

    Update cards with led

--- Progetto tec_web/src/src_control/ControlHome.js
+++ Progetto tec_web/src/src_control/ControlHome.js
@@ -2,37 +2,60 @@ const e = React.createElement;
 
 function Card(props){
     return(
-        e("div",{className: "control_home"},[
-            e("div", {id: "header_card", className:"div_card"},"Nome/Id: "),
-            e("div", {id: "body_card",className:"div_card"}, "Sezione: "),
-            e("div", {id: "body_card",className:"div_card"}, "Punteggio: "),
-            e("div", {id: "footer_card"},[
-                e("input",{type: "button", value: "Chat", style: {"font-size": "13px", "margin-left": "24%", "margin-top": "10%"}}, )
+        e("div",{id: props.id, className: "control_home"},[
+            e("div",{id: "description_card", className: "left_div"},[
+                e("div", {className: "div_card"}, "Name/Id:"), //props.id
+                e("div", {className: "div_card"}, "Section:"), //props.section
+                e("div", {className: "div_card"}, "Points:"), //props.points
+                e("div", {id: "footer_card"},[
+                    e("input",{type: "button", value: "Chat", style: {"font-size": "13px", "margin-left": "24%", "margin-top": "5%"}})
+                ])
+            ]),
+            e("div", {id: "led_card", className: "right_div"}, [
+                e("p",{className: "led_text"}, "Help"),
+                e("span", {id: "led_help"}),
+
+                e("p",{className: "led_text"}, ["Human",e("br"),"Evaluation"]),
+                e("span", {id: "led_valutation"})
             ])
         ])
     )
 }
 
+
 function controlHome(props){
     var arr = [];
+   
+    /*
+    axios.get(`https://api.github.com/search/repositories?q=user:its-hmny&sort=updated`)
+        .then((response) => {
+            response.data.forEach((element) => {
+                arr.push(e(Card, {
+                    key: element.id, 
+                    id: element.id, 
+                    section: element.section, 
+                    points: element.points,
+                    humanE: element.humanE,
+                    chat: element.chat,
+                    other: response.data
+                }))
+            })
 
-    /*fetch(`https://api.github.com/search/repositories?q=user:its-hmny&sort=updated`)
-        .then(response => response.json())
-        .then(data => { 
-            for(let i=0 ; i < data.total_count.lenght; i++)
-                arr.push( e(Card, {key: data.items[i].id}) );
-                
-        }).catch((error) => console.log(error));
-        */
+            return arr;
+            })
+            .then((response) => {
+                response.data.forEach((element) => {
+                    if (element.help == true) document.getElementByClass("element.id").document.getElementById("led_help").classList.add("need_help");
+                    if (element.humanE == true) document.getElementByClass("element.id").document.getElementById("led_chat").classList.add("need_help");
+            .catch((error) => console.log(error));
+    */
 
-       for(let i=0 ; i < 30; i++)
-            arr.push(e(Card,null));
+   for(let i=0 ; i < 30; i++)
+    arr.push(e(Card,null));
 
     return e(React.Fragment, null, [
         e("div",null, arr)
          ]); 
-
-   
 }
 
 export default controlHome;

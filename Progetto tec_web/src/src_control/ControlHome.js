708721d954d2db53272563bceebef5395bee88da
Luca Genova <48278825+Lucajett99@users.noreply.github.com>
2020-11-30 16:07:33 +0100

    Added vertical navbar with card minimization, data download and ranking

--- Progetto tec_web/src/src_control/ControlHome.js
+++ Progetto tec_web/src/src_control/ControlHome.js
@@ -1,4 +1,4 @@
-import ButtonAppBar from './components/ButtonAppBar.js';
+import VerticalBar from './components/VerticalBar.js';
 import CardPlayer from './components/CardPlayer.js'
 
 const {Slide, Paper, IconButton, Icon, TextField, Checkbox} = MaterialUI;
@@ -12,51 +12,7 @@ export default function controlHome(props){
 
     //States
     const [arrayPlayers, setArrayPlayers] =  React.useState([]);
-    const [slide, setSlide] = React.useState(false);
-    const [checked, setChecked] = React.useState(false);
-
-    const handleChange = () => {
-        setChecked(!checked);
-    };
-
-    //Mininimized cards with css
-    React.useEffect(() => {
-        if(checked == true){
-            for(let i = 0; i < 10; i++){
-                let card = document.getElementById("Card"+i);
-                let grid = document.getElementById("Card"+i+"_grid");
-                let data_time = document.getElementsByClassName("MuiCardHeader-content")[i];
-                let avatar = document.getElementsByClassName("MuiCardHeader-root")[i];
-                let icons = document.getElementsByClassName("MuiCardActions-root")[i];
-
-                card.classList.add("card_minimized");
-                grid.classList.add("display_minimized");
-                data_time.classList.add("display_minimized");
-                avatar.classList.add("recenter_avatar");
-                icons.style.padding = "0px";
-                icons.classList.add("recenter_icon");
-            }
-        }
-        else{
-            for(let i = 0; i < 10; i++){
-                let card_min = document.getElementById("Card"+i);
-                let grid_min = document.getElementById("Card"+i+"_grid");
-                let data_time = document.getElementsByClassName("MuiCardHeader-content")[i];
-                let avatar = document.getElementsByClassName("MuiCardHeader-root")[i];
-                let icons = document.getElementsByClassName("MuiCardActions-root")[i];
-
-                if(card_min && grid_min){
-                    card_min.classList.remove("card_minimized");
-                    grid_min.classList.remove("display_minimized");
-                    data_time.classList.remove("display_minimized");
-                    avatar.classList.remove("recenter_avatar");
-                    icons.classList.remove("recenter_icon");
-                    icons.style.padding = "8px";
-                }
-            }
-        }           
-    },[checked]);
-    
+    const [slide, setSlide] = React.useState(false);    
 
     React.useEffect(() => {
         axios.get('http://localhost:8000/status')
@@ -80,9 +36,8 @@ export default function controlHome(props){
     }, [])
 
     return e(React.Fragment, null, [
-        e(ButtonAppBar, "null"),
+        e(VerticalBar, "null"),
         e("div",null, arrayPlayers),
-        e(Checkbox, {checked: checked, onChange: handleChange}),
         e(Slide, {in: slide, direction: "left", id: "slide", children: e(Paper, null, [
             e(IconButton, {children: e(Icon, {children: "close"}), onClick: () => {setSlide(false)}}),
             e("div",{style: {width: "80%", height: "50%", marginLeft: "10%", border: "1px solid grey", borderRadius: "5px"}}), //div di arrivo delle risposte da valutare
---
+++ Progetto tec_web/src/src_control/components/VerticalBar.js
@@ -0,0 +1,80 @@
+const {makeStyles, MenuList, Paper, ListItemIcon, MenuItem, Icon, Checkbox, SvgIcon} = MaterialUI;
+const e = React.createElement;
+
+const useStyles = makeStyles({
+  root: {
+	width: 70,
+	height: '100%',
+	backgroundColor: 'lightGrey',
+	float: 'right',
+	textAlign: 'center'
+  },
+  li: {
+	marginLeft: '7px',
+  },
+  distance: {
+	height: 80,
+  }
+});
+
+
+export default function VerticalBar() {
+	const classes = useStyles();
+	let checked = false;
+
+	const handleMinimized = () =>{
+		checked = !checked;
+		if(checked == true){
+			for(let i = 0; i < 10; i++){
+				let card = document.getElementById("Card"+i);
+				let grid = document.getElementById("Card"+i+"_grid");
+				let data_time = document.getElementsByClassName("MuiCardHeader-content")[i];
+				let avatar = document.getElementsByClassName("MuiCardHeader-root")[i];
+				let icons = document.getElementsByClassName("MuiCardActions-root")[i];
+
+				card.classList.add("card_minimized");
+				grid.classList.add("display_minimized");
+				data_time.classList.add("display_minimized");
+				avatar.classList.add("recenter_avatar");
+				icons.style.padding = "0px";
+				icons.classList.add("recenter_icon");
+			}
+		}
+		else{
+			for(let i = 0; i < 10; i++){
+				let card_min = document.getElementById("Card"+i);
+				let grid_min = document.getElementById("Card"+i+"_grid");
+				let data_time = document.getElementsByClassName("MuiCardHeader-content")[i];
+				let avatar = document.getElementsByClassName("MuiCardHeader-root")[i];
+				let icons = document.getElementsByClassName("MuiCardActions-root")[i];
+
+				if(card_min && grid_min){
+					card_min.classList.remove("card_minimized");
+					grid_min.classList.remove("display_minimized");
+					data_time.classList.remove("display_minimized");
+					avatar.classList.remove("recenter_avatar");
+					icons.classList.remove("recenter_icon");
+					icons.style.padding = "8px";
+				}
+			}
+		}           
+	}
+
+	return (
+		e(Paper, {className: classes.root, children: [
+			e(MenuList, {children: [
+				e(MenuItem, {children: [
+					e(ListItemIcon, {onClick: () => {alert("Download data player")}, className: classes.distance, children:  e(Icon, {style: { color: "black"}, className: classes.li, children: "download"})}),
+				]}),
+				e(MenuItem, {children: [
+					e(ListItemIcon, {onClick: handleMinimized, className: classes.distance, children:  e(SvgIcon, {style: { color: "black"}, className: classes.li, xmlns: "http://www.w3.org/2000/svg", children: [ e("path", {fill: "none", d: "M0 0h24v24H0z"}), e("path", {d: "M22 3l-5 6 3 3h-8V4l3 3 6-5 1 1zM3 22l6-5 3 3v-8H4l3 3-5 6 1 1z"})]})}),
+				]}),
+				e(MenuItem, {children: [
+					e(ListItemIcon, {onClick: () => {alert("Ranking of players")}, className: classes.distance, children:  e(SvgIcon, {style: { color: "black"}, className: classes.li, xmlns: "http://www.w3.org/2000/svg", children: [ e("path", {fill: "none", d: "M0 0h24v24H0z"}), e("path", {d: "M17 10V2H7v9l5 3-1 2H7l3 3-1 3 3-2 3 2-1-3 3-3h-4l-1-2 5-3v-1zm-4 2l-1 1-1-1V3h2v9z"})]})}),
+				]}),
+			]})
+		]})
+	);
+
+	
+}

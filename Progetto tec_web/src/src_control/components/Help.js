f7a193eff77611a5de7a9fe01bdd4f203aeba260
Luca Genova <48278825+Lucajett99@users.noreply.github.com>
2021-01-18 02:55:25 +0100

    Added help and Evaluation

---
+++ Progetto tec_web/src/src_control/components/Help.js
@@ -0,0 +1,31 @@
+import { isEnter } from "../../../utils.js";
+const { Box, TextField, Icon, IconButton } = MaterialUI;
+const e = React.createElement;
+
+export default function Help(props) {
+
+    const onSend = () => {
+        const texfield = document.getElementById("help_" + props.id);
+        const answer = texfield.value;
+        texfield.value = "";
+        props.socket.emit('help-from-player', { answer });
+        //something
+        const tmp = _.cloneDeep(props.arrayHelps);
+        const player = props.player;
+        const array = props.arrayHelps[player];
+        array.forEach((item, index) => {
+            if(item.props.id == props.id)
+                tmp[player].splice(index, 1)
+        })
+        props.setArrayHelps(tmp);
+    }
+    
+    return(
+        e(React.Fragment, null, [
+            e("div", {style: {backgroundColor: "#3f51b5", height: 70, width: "80%", border: "1px solid grey"}}, [ `Question: ${props.question}` ]),
+            e(TextField, {onKeyDown: () => {isEnter(event)? onSend() : null}, id: "help_" + props.id, key: "3", variant: "outlined", style: {width: "80%", marginBottom: 20}, InputProps: {endAdornment:
+                e(IconButton, {onClick: onSend, key: "4", children: e(Icon, {children: "send"})}), style: {fontSize: "14pt"}}}
+            )
+        ])
+    );
+}

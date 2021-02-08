92ccdb30dfacbd1944869e90e596a83563264283
simon <simoneboldrini1999@gmail.com>
2021-02-08 15:04:22 +0100

    rename index2 in player

---
+++ Progetto tec_web/src2/player.js
@@ -0,0 +1,13 @@
+const e = React.createElement;
+
+import App from './App2.js'
+
+ReactDOM.render(
+    e(
+        React.StrictMode, 
+        {key: "app1s"}, 
+        e(App, {key: "app2"})
+    ),
+    document.getElementById('root2')
+);
+
